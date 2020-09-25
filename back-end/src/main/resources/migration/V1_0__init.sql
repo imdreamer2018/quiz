22b29e5c3c6225caafd66a36bed57ae2d0c5ec4a
imdreamer <743295483@qq.com>
2020-09-25 17:45:27 +0800

    [yangqian] implement: flyaway order table.

--- back-end/src/main/resources/migration/V1_0__init.sql
+++ back-end/src/main/resources/migration/V1_0__init.sql
@@ -5,3 +5,10 @@ CREATE table product (
     unit varchar(100) not null,
     img_link varchar(100) not null
 ) engine=InnoDB default charset=utf8;
+
+CREATE table product_order (
+    id int(11) not null primary key auto_increment,
+    order_num int(11) not null,
+    product_id int(11) not null,
+    foreign key (product_id) references product(id) ON DELETE CASCADE
+) engine=InnoDB default charset=utf8;
---
+++ back-end/src/main/resources/migration/V1_2__create_orders.sql
@@ -0,0 +1,6 @@
+INSERT INTO product_order(order_num, product_id)
+VALUES (1, 1),
+(2, 2),
+(3, 4),
+(4, 5)
+
