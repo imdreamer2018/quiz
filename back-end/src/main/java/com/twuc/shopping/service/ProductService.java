9da70aa82c29cf5904abe32f111597a9a1595741
imdreamer <743295483@qq.com>
2020-09-25 14:31:33 +0800

    [yangqian] implement: should return products when get all products service test.

--- back-end/src/main/java/com/twuc/shopping/service/ProductService.java
+++ back-end/src/main/java/com/twuc/shopping/service/ProductService.java
@@ -1,14 +1,27 @@
 package com.twuc.shopping.service;
 
+import com.twuc.shopping.dto.ProductResponse;
+import com.twuc.shopping.entity.ProductEntity;
 import com.twuc.shopping.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    final ProductRepository productRepository;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    public ProductResponse<List<ProductEntity>> getAllProducts() {
+        List<ProductEntity> productEntities = productRepository.findAll();
+        ProductResponse<List<ProductEntity>> response = new ProductResponse<>();
+        response.setCode(200);
+        response.setMessage("get all products success!");
+        response.setData(productEntities);
+        return response;
     }
 }
--- back-end/src/test/java/com/twuc/shopping/ServiceTests/ProductServiceTest.java
+++ back-end/src/test/java/com/twuc/shopping/ServiceTests/ProductServiceTest.java
@@ -1,11 +1,22 @@
 package com.twuc.shopping.ServiceTests;
 
+import com.twuc.shopping.dto.ProductResponse;
+import com.twuc.shopping.entity.ProductEntity;
 import com.twuc.shopping.repository.ProductRepository;
 import com.twuc.shopping.service.ProductService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @SpringBootTest
@@ -22,5 +33,19 @@ public class ProductServiceTest {
         productService = new ProductService(productRepository);
     }
 
+    @Test
+    void should_return_products_info_when_get_products_success() {
+        ProductEntity product = ProductEntity.builder()
+                .name("雪碧")
+                .price(3)
+                .unit("瓶")
+                .imgLink("http://11.com")
+                .build();
+        List<ProductEntity> products = new ArrayList<>();
+        products.add(product);
+        when(productRepository.findAll()).thenReturn(products);
+        ProductResponse<List<ProductEntity>> response = productService.getAllProducts();
+        assertEquals("get all products success!", response.getMessage());
+    }
 
 }
