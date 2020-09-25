fa93603f56334f98306d23be6d62b265dff071f4
imdreamer <743295483@qq.com>
2020-09-25 15:45:58 +0800

    [yangqian] init: project structure including Shop Header Products and Footer.

---
+++ front-end/src/app/components/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Shop extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Shop;
