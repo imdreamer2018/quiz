831cd9a0d78b0d28c6daccae8487db7735a661cd
imdreamer <743295483@qq.com>
2020-09-25 15:21:20 +0800

    [yangqian] implement: BaseProductException.

---
+++ back-end/src/main/java/com/twuc/shopping/exception/BaseProductException.java
@@ -0,0 +1,7 @@
+package com.twuc.shopping.exception;
+
+public class BaseProductException extends RuntimeException {
+    public BaseProductException(String message) {
+        super(message);
+    }
+}
